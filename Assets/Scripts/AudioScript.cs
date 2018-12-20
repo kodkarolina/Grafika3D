7d992362d181e859613bbe75f18f304950967875
Karolina Wisniewska <kodkarolina@gmail.com>
2018-12-20 20:22:42 +0100

    Adding android apk and sound

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour {
+
+	public AudioSource audioSource;
+    public double minimumVelocity;
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody>();
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(rb.velocity.magnitude >= minimumVelocity && !audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+        else if(rb.velocity.magnitude < minimumVelocity && audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+        
+
+	}
+}
--- Assets/Scripts/MazeSteer.cs
+++ Assets/Scripts/MazeSteer.cs
@@ -6,7 +6,8 @@ using UnityEngine.UI;
 using System.Threading;
 using System.Globalization;
 
-public class MazeSteer : MonoBehaviour {
+public class MazeSteer : MonoBehaviour
+{
 
 
     Text text;
@@ -18,20 +19,21 @@ public class MazeSteer : MonoBehaviour {
 
     public Vector3 currentRot;
     public bool KeyboardEnabled;
-	public float rotateSpeedKeyboard = 1f;
-	public float rotateSpeedMouse = 1f;
-	public float fastRotateSpeed = 30f;
-	public float MinimumX = -15F;
-	public float MaximumX = 15F;
-	public GameObject ControllText;
+    public float rotateSpeedKeyboard = 1f;
+    public float rotateSpeedMouse = 1f;
+    public float fastRotateSpeed = 30f;
+    public float MinimumX = -15F;
+    public float MaximumX = 15F;
+    public GameObject ControllText;
 
     private volatile float axis_x;
     private volatile float axis_y;
     private volatile float axis_z;
     private Quaternion targetRot;
 
-	void Start () {
-		ControllText.SetActive (false);
+    void Start()
+    {
+        ControllText.SetActive(false);
 
         m_continue = true;
         text = GetComponent<Text>();
@@ -42,30 +44,39 @@ public class MazeSteer : MonoBehaviour {
         readThread.Start();
     }
 
-	void Update () {
-        
-        if (KeyboardEnabled) {
-			RotateWithKeyboard ();
-		} else {
-		    RotateWithPhone ();
-		}
-		if(Input.GetKeyDown(KeyCode.Space)){
-			ToggleControlls ();
-		}
-	}
+    void Update()
+    {
 
-	void RotateWithMouse(){
+        if (KeyboardEnabled)
+        {
+            RotateWithKeyboard();
+        }
+        else
+        {
+            RotateWithPhone();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ToggleControlls();
+        }
+    }
 
-		float xRot = Input.GetAxis ("Mouse X") * rotateSpeedMouse;
-		float zRot = Input.GetAxis ("Mouse Y") * rotateSpeedMouse;
-		Rotate (-xRot, -zRot);
+    void RotateWithMouse()
+    {
 
-	}
+        float xRot = Input.GetAxis("Mouse X") * rotateSpeedMouse;
+        float zRot = Input.GetAxis("Mouse Y") * rotateSpeedMouse;
+        Rotate(-xRot, -zRot);
 
-    void RotateWithPhone(){
+    }
+
+    void RotateWithPhone()
+    {
         float xRotation = axis_x;
         float zRotation = axis_z;
-        Rotate(xRotation, zRotation);
+        axis_y = Mathf.Clamp(axis_y, MinimumX, MaximumX);
+        axis_z = Mathf.Clamp(axis_z, MinimumX, MaximumX);
+        transform.rotation = Quaternion.Euler(axis_y, 0, axis_z);
     }
 
     void RotateWithKeyboard()
@@ -92,38 +103,49 @@ public class MazeSteer : MonoBehaviour {
         }
     }
 
-    void Rotate(float xRot, float zRot){
+    void Rotate(float xRot, float zRot)
+    {
 
-		targetRot = transform.rotation * Quaternion.Euler (xRot, 0f, zRot);
+        targetRot = transform.rotation * Quaternion.Euler(xRot, 0f, zRot);
 
-		float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan (targetRot.x);
-		angleX = Mathf.Clamp (angleX, MinimumX, MaximumX);
-		targetRot.x = Mathf.Tan (0.5f * Mathf.Deg2Rad * angleX);
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(targetRot.x);
+        angleX = Mathf.Clamp(angleX, MinimumX, MaximumX);
+        targetRot.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
 
-		float angleZ = 2.0f * Mathf.Rad2Deg * Mathf.Atan (targetRot.z);
-		angleZ = Mathf.Clamp (angleZ, MinimumX, MaximumX);
-		targetRot.z = Mathf.Tan (0.5f * Mathf.Deg2Rad * angleZ);
+        float angleZ = 2.0f * Mathf.Rad2Deg * Mathf.Atan(targetRot.z);
+        angleZ = Mathf.Clamp(angleZ, MinimumX, MaximumX);
+        targetRot.z = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleZ);
 
-		targetRot.y = 0f;
+        targetRot.y = 0f;
 
-		transform.rotation = targetRot;
-	}
+        transform.rotation = targetRot;
+
+    }
 
     void Read()
     {
+
+
         while (m_continue)
         {
+            try
+            {
+                message = port.ReadTo("q");
+                var axes = message.Split(' ');
+                string axis_x_string = axes[0];
+                string axis_y_string = axes[1];
+                string axis_z_string = axes[2];
 
-            message = port.ReadTo("q");
-            var axes = message.Split(' ');
-            string axis_x_string = axes[0];
-            string axis_y_string = axes[1];
-            string axis_z_string = axes[2];
+                axis_x = float.Parse(axis_x_string, CultureInfo.InvariantCulture.NumberFormat);
+                axis_y = float.Parse(axis_y_string, CultureInfo.InvariantCulture.NumberFormat);
+                axis_z = float.Parse(axis_z_string, CultureInfo.InvariantCulture.NumberFormat);
+                Debug.Log(axes);
+            }
+            catch
+            {
+                Debug.Log("PROBLEM");
+            }
 
-            axis_x = float.Parse(axis_x_string, CultureInfo.InvariantCulture.NumberFormat);
-            axis_y = float.Parse(axis_y_string, CultureInfo.InvariantCulture.NumberFormat);
-            axis_z = float.Parse(axis_z_string, CultureInfo.InvariantCulture.NumberFormat);
-            Debug.Log(axes);
         }
     }
 
@@ -133,12 +155,12 @@ public class MazeSteer : MonoBehaviour {
         if (KeyboardEnabled)
         {
             KeyboardEnabled = false;
-            ControllText.GetComponent<TextMesh>().text = "Mouse Controled";
+            ControllText.GetComponent<TextMesh>().text = " "; // Phone Controlled
         }
         else
         {
             KeyboardEnabled = true;
-            ControllText.GetComponent<TextMesh>().text = "Keyboard Controled";
+            ControllText.GetComponent<TextMesh>().text = " "; //keyboard Controlled
         }
         StartCoroutine(ToggleActivity());
     }
